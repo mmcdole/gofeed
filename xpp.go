fb6534e38dfa37bbd24c7eeb39cd91f13820d7af
Matthew McDole <matthew.d.mcdole@ehi.com>
2016-01-26 00:37:07 -0600

    More work on xpp

--- xpp.go
+++ xpp.go
@@ -47,20 +47,32 @@ func (p *XMLPullParser) NextTag() (XMLEventType, error) {
 }
 
 func (p *XMLPullParser) NextToken() (event XMLEventType, err error) {
+	// Clear any state held for the previous token
+	p.resetTokenState()
+
 	tok, err := p.decoder.Token()
 	if err != nil {
+		if err != io.EOF {
+			return
+		}
+
+		// XML decoder returns the EOF as an error
+		// but we want to return it as a valid
+		// EndDocument token instead
+		p.token = nil
+		err = nil
+		event = p.processEndDocument()
 		return
 	}
 
 	p.token = tok
-
 	switch tt := tok.(type) {
 	case xml.StartElement:
-		p.processStartToken(tt)
+		event = p.processStartToken(tt)
 	case xml.EndElement:
-		p.processEndToken(tt)
+		event = p.processEndToken(tt)
 	case xml.CharData:
-		p.processTextToken(tt)
+		event = p.processTextToken(tt)
 	}
 }
 
@@ -91,7 +103,7 @@ func (p *XMLPullParser) Matches(event XMLEventType, namespace *string, name *str
 	return p.Event == event && (namespace == nil || p.Namespace == namespace) && (name == nil || p.Name == name)
 }
 
-func (p *XMLPullParser) processStartToken(t *xml.StartElement) {
+func (p *XMLPullParser) processStartToken(t *xml.StartElement) XMLEventType {
 	p.Depth++
 	p.Event = StartTag
 	p.Attrs = t.Attr
@@ -99,13 +111,24 @@ func (p *XMLPullParser) processStartToken(t *xml.StartElement) {
 	p.Space = t.Space
 }
 
-func (p *XMLPullParser) processEndToken(t *xml.EndElement) {
+func (p *XMLPullParser) processEndToken(t *xml.EndElement) XMLEventType {
 	p.Depth--
 	p.Event = EndTag
 	p.Name = t.Name
 }
 
-func (p *XMLPullParser) processTextToken(t *xml.CharData) {
+func (p *XMLPullParser) processTextToken(t *xml.CharData) XMLEventType {
 	p.Event = Text
 	p.Text = string([]byte(t))
+}
+
+func (p *XMLPullParser) processEndDocument() XMLEventType {
+	p.Event = EndDocument
+}
+
+func (p *XMLPullParser) resetTokenState() {
+	p.Attrs = nil
+	p.Name = ""
+	p.Space = ""
+	p.Text = ""
 }
