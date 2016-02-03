2cb30007c8e25e4735eb808f816125785a9e3ca5
Matthew McDole <matthew.d.mcdole@ehi.com>
2016-02-03 16:54:13 -0600

    Begin to add documentation

--- atom/translator.go
+++ atom/translator.go
@@ -4,10 +4,18 @@ import (
 	"github.com/mmcdole/gofeed/feed"
 )
 
+// Translator converts an atom.Feed struct
+// into the generic feed.Feed struct
 type Translator interface {
 	Translate(atom *Feed) *feed.Feed
 }
 
+// DefaulTranslator converts an atom.Feed struct
+// into the generic feed.Feed struct.
+//
+// This default implementation defines a set of
+// mapping rules between atom.Feed -> feed.Feed
+// for each of the fields in feed.Feed.
 type DefaultTranslator struct{}
 
 func (t *DefaultTranslator) Translate(atom *Feed) *feed.Feed {
--- rss/translator.go
+++ rss/translator.go
@@ -4,10 +4,18 @@ import (
 	"github.com/mmcdole/gofeed/feed"
 )
 
+// Translator converts an rss.Feed struct
+// into the generic feed.Feed struct
 type Translator interface {
 	Translate(rss *Feed) *feed.Feed
 }
 
+// DefaulTranslator converts an rss.Feed struct
+// into the generic feed.Feed struct.
+//
+// This default implementation defines a set of
+// mapping rules between rss.Feed -> feed.Feed
+// for each of the fields in feed.Feed.
 type DefaultTranslator struct{}
 
 func (t *DefaultTranslator) Translate(rss *Feed) *feed.Feed {
