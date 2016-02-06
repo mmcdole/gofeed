277a0f6f4da376af35d5936964b3b9e9b4cc4a83
Matthew McDole <matthew.d.mcdole@ehi.com>
2016-02-05 21:36:42 -0600

    Starting to flesh out atom model

--- atom/feed.go
+++ atom/feed.go
@@ -1,11 +1,66 @@
 package atom
 
-import ()
+import (
+	"time"
+)
 
 type Feed struct {
-	Title string
+	Title         string
+	ID            string
+	Updated       string
+	UpdatedParsed *time.Time
+	Subtitle      string
+	Link          string
+	Generator     Generator
+	Icon          string
+	Logo          string
+	Rights        string
+	Contributors  []Person
+	Authors       []Person
+	Categories    []Category
+	Source        string
+	Version       string
 }
 
 type Entry struct {
-	Title string
+	Title           string
+	ID              string
+	Link            Link
+	Published       string
+	PublishedParsed *time.Time
+	Updated         string
+	UpdatedParsed   *time.Time
+	Content         string
+}
+
+type Category struct {
+	Term   string
+	Scheme string
+	Label  string
+}
+
+type Person struct {
+	Name  string
+	Email string
+	URI   string
+}
+
+type Link struct {
+	Rel      string
+	Type     string
+	Href     string
+	Hreflang string
+	Title    string
+	Length   string
+}
+
+type Content struct {
+	Src  string
+	Type string
+}
+
+type Generator struct {
+	Value   string
+	URI     string
+	Version string
 }
