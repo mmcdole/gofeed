6ccd8620e1110c07992bb4d80c11e9bb57670fed
mmcdole <simucal@gmail.com>
2017-08-30 08:58:11 -0500

    Fix extension parsing of consecutive text nodes.  Also, carry ITunesExt and DublinCoreExt to gofeed.Feed (#79)

--- feed.go
+++ feed.go
@@ -11,25 +11,27 @@ import (
 // and rss.Feed gets translated to. It represents
 // a web feed.
 type Feed struct {
-	Title           string            `json:"title,omitempty"`
-	Description     string            `json:"description,omitempty"`
-	Link            string            `json:"link,omitempty"`
-	FeedLink        string            `json:"feedLink,omitempty"`
-	Updated         string            `json:"updated,omitempty"`
-	UpdatedParsed   *time.Time        `json:"updatedParsed,omitempty"`
-	Published       string            `json:"published,omitempty"`
-	PublishedParsed *time.Time        `json:"publishedParsed,omitempty"`
-	Author          *Person           `json:"author,omitempty"`
-	Language        string            `json:"language,omitempty"`
-	Image           *Image            `json:"image,omitempty"`
-	Copyright       string            `json:"copyright,omitempty"`
-	Generator       string            `json:"generator,omitempty"`
-	Categories      []string          `json:"categories,omitempty"`
-	Extensions      ext.Extensions    `json:"extensions,omitempty"`
-	Custom          map[string]string `json:"custom,omitempty"`
-	Items           []*Item           `json:"items"`
-	FeedType        string            `json:"feedType"`
-	FeedVersion     string            `json:"feedVersion"`
+	Title           string                   `json:"title,omitempty"`
+	Description     string                   `json:"description,omitempty"`
+	Link            string                   `json:"link,omitempty"`
+	FeedLink        string                   `json:"feedLink,omitempty"`
+	Updated         string                   `json:"updated,omitempty"`
+	UpdatedParsed   *time.Time               `json:"updatedParsed,omitempty"`
+	Published       string                   `json:"published,omitempty"`
+	PublishedParsed *time.Time               `json:"publishedParsed,omitempty"`
+	Author          *Person                  `json:"author,omitempty"`
+	Language        string                   `json:"language,omitempty"`
+	Image           *Image                   `json:"image,omitempty"`
+	Copyright       string                   `json:"copyright,omitempty"`
+	Generator       string                   `json:"generator,omitempty"`
+	Categories      []string                 `json:"categories,omitempty"`
+	DublinCoreExt   *ext.DublinCoreExtension `json:"dcExt,omitempty"`
+	ITunesExt       *ext.ITunesFeedExtension `json:"itunesExt,omitempty"`
+	Extensions      ext.Extensions           `json:"extensions,omitempty"`
+	Custom          map[string]string        `json:"custom,omitempty"`
+	Items           []*Item                  `json:"items"`
+	FeedType        string                   `json:"feedType"`
+	FeedVersion     string                   `json:"feedVersion"`
 }
 
 func (f Feed) String() string {
@@ -41,21 +43,23 @@ func (f Feed) String() string {
 // and rss.Item gets translated to.  It represents
 // a single entry in a given feed.
 type Item struct {
-	Title           string            `json:"title,omitempty"`
-	Description     string            `json:"description,omitempty"`
-	Content         string            `json:"content,omitempty"`
-	Link            string            `json:"link,omitempty"`
-	Updated         string            `json:"updated,omitempty"`
-	UpdatedParsed   *time.Time        `json:"updatedParsed,omitempty"`
-	Published       string            `json:"published,omitempty"`
-	PublishedParsed *time.Time        `json:"publishedParsed,omitempty"`
-	Author          *Person           `json:"author,omitempty"`
-	GUID            string            `json:"guid,omitempty"`
-	Image           *Image            `json:"image,omitempty"`
-	Categories      []string          `json:"categories,omitempty"`
-	Enclosures      []*Enclosure      `json:"enclosures,omitempty"`
-	Extensions      ext.Extensions    `json:"extensions,omitempty"`
-	Custom          map[string]string `json:"custom,omitempty"`
+	Title           string                   `json:"title,omitempty"`
+	Description     string                   `json:"description,omitempty"`
+	Content         string                   `json:"content,omitempty"`
+	Link            string                   `json:"link,omitempty"`
+	Updated         string                   `json:"updated,omitempty"`
+	UpdatedParsed   *time.Time               `json:"updatedParsed,omitempty"`
+	Published       string                   `json:"published,omitempty"`
+	PublishedParsed *time.Time               `json:"publishedParsed,omitempty"`
+	Author          *Person                  `json:"author,omitempty"`
+	GUID            string                   `json:"guid,omitempty"`
+	Image           *Image                   `json:"image,omitempty"`
+	Categories      []string                 `json:"categories,omitempty"`
+	Enclosures      []*Enclosure             `json:"enclosures,omitempty"`
+	DublinCoreExt   *ext.DublinCoreExtension `json:"dcExt,omitempty"`
+	ITunesExt       *ext.ITunesItemExtension `json:"itunesExt,omitempty"`
+	Extensions      ext.Extensions           `json:"extensions,omitempty"`
+	Custom          map[string]string        `json:"custom,omitempty"`
 }
 
 // Person is an individual specified in a feed
