fb0f6e8049fc4b06051f43781dff05def9126bb7
Alex R <alexei.rudenko@gmail.com>
2019-04-11 22:11:04 +0200

    Add CDATA test cases

--- internal/shared/parseutils_test.go
+++ internal/shared/parseutils_test.go
@@ -66,6 +66,55 @@ func TestStripCDATA(t *testing.T) {
 		{"]]>", "]]>"},
 		{"<![CDATA[", "<![CDATA["},
 		{"<![CDATA[testtest", "<![CDATA[testtest"},
+		{`<![CDATA[
+    Since this is a CDATA section
+    I can use all sorts of reserved characters
+    like > < " and &
+    or write things like
+    <foo></bar>
+    but my document is still well formed!
+]]>`, `
+    Since this is a CDATA section
+    I can use all sorts of reserved characters
+    like > < " and &
+    or write things like
+    <foo></bar>
+    but my document is still well formed!
+`},
+		{`<![CDATA[
+Within this Character Data block I can
+use double dashes as much as I want (along with <, &, ', and ")
+*and* %MyParamEntity; will be expanded to the text
+"Has been expanded" ... however, I can't use
+the CEND sequence. If I need to use CEND I must escape one of the
+brackets or the greater-than sign using concatenated CDATA sections.
+]]>`, `
+Within this Character Data block I can
+use double dashes as much as I want (along with <, &, ', and ")
+*and* %MyParamEntity; will be expanded to the text
+"Has been expanded" ... however, I can't use
+the CEND sequence. If I need to use CEND I must escape one of the
+brackets or the greater-than sign using concatenated CDATA sections.
+`},
+		// 		{`<![CDATA[ test ]]><!--
+		// Within this comment I can use ]]>
+		// and other reserved characters like <
+		// &, ', and ", but %MyParamEntity; will not be expanded
+		// (if I retrieve the text of this node it will contain
+		// %MyParamEntity; and not "Has been expanded")
+		// and I can't place two dashes next to each other.
+		// -->`, ` test <!--
+		// Within this comment I can use ]]>
+		// and other reserved characters like <
+		// &, ', and ", but %MyParamEntity; will not be expanded
+		// (if I retrieve the text of this node it will contain
+		// %MyParamEntity; and not "Has been expanded")
+		// and I can't place two dashes next to each other.
+		// -->`,
+		// 		},
+		{`An example of escaped CENDs`, `An example of escaped CENDs`},
+		{`<![CDATA[This text contains a CEND ]]]]><![CDATA[>]]>`, `This text contains a CEND ]]>`},
+		{`<![CDATA[This text contains a CEND ]]]><![CDATA[]>]]>`, `This text contains a CEND ]]>`},
 	}
 
 	for _, test := range tests {
